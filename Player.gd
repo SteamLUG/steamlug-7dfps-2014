7b3273dd25bb0f18194c46c0450b9a1ee0957253
Alejandro Seguí <alesegdia@gmail.com>
2014-10-30 21:05:27 +0100

    lights and shadows

--- Player.gd
+++ Player.gd
@@ -1,7 +1,8 @@
 
 extends RigidBody
 
-var speed = 4
+var walk_speed = 4
+var jump_force = 100
 
 func _ready():
 	print("hahahaha")
@@ -9,17 +10,22 @@ func _ready():
 
 func _integrate_forces(state):
 
+	# Handle movement
 	var lv = state.get_linear_velocity()
-
 	if( Input.is_action_pressed("player_forward") ):
-		lv.z = -speed
+		lv.z = -walk_speed
 	elif( Input.is_action_pressed("player_backwards") ):
-		lv.z = speed
-
+		lv.z = walk_speed
 	if( Input.is_action_pressed("player_left") ):
-		lv.x = -speed
+		lv.x = -walk_speed
 	elif( Input.is_action_pressed("player_right") ):
-		lv.x = speed
+		lv.x = walk_speed
+
+	# Handle jump
+	print( state.get_contact_count())
+	var onfloor = state.get_contact_count()
+	if Input.is_action_pressed("player_jump"):
+		state.add_force(Vector3(0,jump_force,0), Vector3(0,1,0))
 
 	state.set_linear_velocity(lv)
 	state.integrate_forces()
