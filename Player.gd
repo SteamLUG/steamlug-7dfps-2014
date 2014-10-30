e39cc66714ccdf1932dfefea6654b3d9e7102919
Alejandro Seguí <alesegdia@gmail.com>
2014-10-30 22:19:39 +0100

    fixed jump, added testing texture

--- Player.gd
+++ Player.gd
@@ -1,30 +1,34 @@
 
 extends RigidBody
 
-var walk_speed = 4
-var jump_force = 100
+var walk_speed = 1
+var jump_force = 3000
+var max_speed = 6
 
 func _ready():
-	print("hahahaha")
 	pass
+
 
 func _integrate_forces(state):
 
 	# Handle movement
 	var lv = state.get_linear_velocity()
 	if( Input.is_action_pressed("player_forward") ):
-		lv.z = -walk_speed
+		lv.z -= walk_speed
 	elif( Input.is_action_pressed("player_backwards") ):
-		lv.z = walk_speed
+		lv.z += walk_speed
 	if( Input.is_action_pressed("player_left") ):
-		lv.x = -walk_speed
+		lv.x -= walk_speed
 	elif( Input.is_action_pressed("player_right") ):
-		lv.x = walk_speed
+		lv.x += walk_speed
+
+	lv.x = min( max_speed, abs(lv.x) ) * sign(lv.x)
+	lv.z = min( max_speed, abs(lv.z) ) * sign(lv.z)
 
 	# Handle jump
 	print( state.get_contact_count())
 	var onfloor = state.get_contact_count()
-	if Input.is_action_pressed("player_jump"):
+	if onfloor and Input.is_action_pressed("player_jump"):
 		state.add_force(Vector3(0,jump_force,0), Vector3(0,1,0))
 
 	state.set_linear_velocity(lv)
