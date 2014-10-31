0448f69ef35ff020f780a12ad43bc1b5c064a075
HER0_01 <aconrad103@gmail.com>
2014-10-31 02:11:32 -0500

    Added bare lantern script

---
+++ lantern.gd
@@ -0,0 +1,13 @@
+# This should make the light flicker subtly, as lanterns do.
+
+extends SpotLight
+
+# member variables here, example:
+# var a=2
+# var b="textvar"
+
+func _ready():
+	# Initalization here
+	pass
+
+
