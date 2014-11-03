2417412f564ee0f00b608e4e8699bc397e0c7866
HER0_01 <aconrad103@gmail.com>
2014-11-03 14:27:04 -0600

    Restructure filesystem

---
+++ main_menu/main_menu.gd
@@ -0,0 +1,11 @@
+
+extends Control
+
+func _ready():
+	pass
+
+func _on_goto_test_pressed():
+	get_node("/root/scene_switcher").goto_scene("res://test/test.xscn")
+
+func _on_goto_multi_pressed():
+	get_node("/root/scene_switcher").goto_scene("res://lobby/lobby_testing.xscn")
