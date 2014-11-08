99d5cc46ede350c4d87e68d717884aa336c2aaf4
Rob Bean <papabean@gmail.com>
2014-11-08 00:22:16 -0800

    begin setup of lantern oil usage

--- player/lantern.gd
+++ player/lantern.gd
@@ -5,9 +5,16 @@ extends SpotLight
 # member variables here, example:
 # var a=2
 # var b="textvar"
+## Begin work on oil status
+export(int, 0, 100) var oil = 100			# Start with full oil
 
 func _ready():
-	# Initalization here
 	pass
 
-
+func _process():
+	# TODO: Add code to set brightness levels based on oil level
+	# TODO: Setup setter/getters and/or functions to change oil level based on usage
+	# TODO: or movement
+	# TODO: Check Godot documentation since it seems _process only gets delta of time
+	# TODO: since last frame
+	pass
