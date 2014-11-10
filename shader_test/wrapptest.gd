27d1c3d8df044351a6ed7e12f101d9cbcdf309d7
Alejandro Seguí <alesegdia@gmail.com>
2014-11-10 22:44:30 +0100

    Can't set shader uniforms. Testing results are commented in code.

--- shader_test/wrapptest.gd
+++ shader_test/wrapptest.gd
@@ -1,15 +1,30 @@
 
 extends Node
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
+func set_fixedmat():
+	# working for fixed material
+	# remember changing quad material to FIXEDMATERIAL!
+	var tex = get_node("Viewport").get_render_target_texture()
+	get_node("Quad").get_material_override().set_texture(FixedMaterial.PARAM_DIFFUSE,tex)
+
+func set_shadermat():
+	# not working...
+	# remember changing quad material to SHADERMATERIAL!
+	# It seems "set" property just don't fail when the property doesn't exist,
+	# but still not working for setting any kind of uniform
+	var tex = get_node("Viewport").get_render_target_texture()
+	get_node("Quad").get_material_override().set("tehscreen", tex)
+
+	var mat = get_node("Quad").get_material_override()
+	get_node("Quad").set_material_override(mat)
 
 func _ready():
 	# Initalization here
-
-	var tex = get_node("Viewport").get_render_target_texture()
-	get_node("Quad").get_material_override().set_texture(FixedMaterial.PARAM_DIFFUSE,tex)
+	#set_shadermat()
+	set_fixedmat()
+	set_process(true)
 	pass
 
-
+func _process(dt):
+	#set_shadermat()
+	set_fixedmat()
