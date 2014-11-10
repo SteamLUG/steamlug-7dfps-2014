494afde33284c557a8a2bebaaa5eeb2b5e742ac6
HER0_01 <aconrad103@gmail.com>
2014-11-09 23:32:33 -0600

    Fix for network files move

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -8,4 +8,4 @@ func _on_goto_test_pressed():
 	get_node("/root/scene_switcher").goto_scene("res://test/test.xscn")
 
 func _on_goto_multi_pressed():
-	get_node("/root/scene_switcher").goto_scene("res://lobby/lobby_testing.xscn")
+	get_node("/root/scene_switcher").goto_scene("res://net/lobby.xscn")
