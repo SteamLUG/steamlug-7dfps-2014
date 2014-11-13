7d419169b1de1e527002b7ac1d07c7c73ad46074
Rob Bean <papabean@gmail.com>
2014-11-12 18:59:18 -0800

    Added movement code to ghost

--- ghost/ghost.gd
+++ ghost/ghost.gd
@@ -5,6 +5,21 @@ var revealed
 var mesh
 var killbox
 
+var exit_game = false
+var win_hsize
+
+# From player.gd
+export var walk_speed = 10
+export var jump_force = 10
+
+# Camera
+var camera
+var rotation = Vector2(0,0)
+var max_pitch = deg2rad(80)
+
+# Speed modifier
+var speed_mod
+
 func _ready():
 	revealed = false
 	mesh = get_node("chumpus")
@@ -15,7 +30,80 @@ func _ready():
 	get_node("/root/Map").add_child(killbox)
 	killbox.set_ghost(self)
 	
+	# Setup camera
+	var rot = get_rotation()
+	rotation.x = rot.y
+	rot.y = 0
+	set_rotation(rot)
+	camera = get_node("Camera")
+	win_hsize = OS.get_video_mode_size()/2
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	set_process_input(true)
+	
 	set_process(true)
+
+func _integrate_forces(state):
+	
+	# Reset mouse
+	Input.warp_mouse_pos(win_hsize)
+	
+	# Get directions
+	var forward = camera.get_transform().basis[2]
+	forward = Vector3(forward.x, 0, forward.z).normalized()
+	var strafe = camera.get_transform().basis[0]
+	strafe = Vector3(strafe.x, 0, strafe.z).normalized()
+	
+	# Quit game
+	if exit_game:
+		OS.get_main_loop().quit()
+	if Input.is_action_pressed("game_quit"):
+		print("quit")
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		exit_game = true
+
+	# Handle movement
+	var lv = state.get_linear_velocity()
+	if Input.is_action_pressed("player_forward"):
+		lv -= forward
+	elif Input.is_action_pressed("player_backwards"):
+		lv += forward
+	if Input.is_action_pressed("player_left"):
+		lv -= strafe
+	elif Input.is_action_pressed("player_right"):
+		lv += strafe
+	
+	# Clamp speed
+	var tmp = Vector3( lv.x, 0, lv.z )
+	if tmp.length() > walk_speed:
+		tmp = tmp.normalized() * walk_speed
+		lv.x = tmp.x
+		lv.z = tmp.z
+	
+	# Handle jump
+	var onfloor = state.get_contact_count()
+	if onfloor and Input.is_action_pressed("player_jump"):
+		lv.y = jump_force
+		# if we use forces, jump_force should be over 9000
+		#state.add_force(Vector3(0,jump_force,0), Vector3(0,1,0))
+	
+	# Apply walk velocity
+	state.set_linear_velocity(lv)
+	
+	# Clamp pitch
+	rotation.y = min( max_pitch, abs(rotation.y) ) * sign( rotation.y )
+	
+	# Rotate camera by using transform
+	var rot = Matrix3().rotated(Vector3(0,1,0), rotation.x).rotated(Vector3(1,0,0),rotation.y)
+	var transf = camera.get_transform()
+	transf.basis = rot
+	camera.set_transform(transf)
+	
+	# Commit!
+	state.integrate_forces()
+
+func _input(ev):
+	if ev.type == InputEvent.MOUSE_MOTION:
+		rotation += (ev.pos - win_hsize) * 0.001
 
 # Called by lantern when raycast hits ghost.
 func reveal():
